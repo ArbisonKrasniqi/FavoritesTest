89b3bfad9a849111f762287404301c7eb164b414
ArbisonKrasniqi <ak64951@ubt-uni.net>
2023-11-22 15:39:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let favorites;
+localStorage.setItem('sharedArray', JSON.stringify(favorites));
+
+function updateArray() {
+    let currentArray = JSON.parse(lcoalStorage.getItem('favorites')) || [];
+    currentArray = favorites;
+    localStorage.setItem('sharedArray', JSON.stringify(favorites));
+}
+function addToFavorite(element) {
+    let parent = element.parentElement;
+    parent.classList.add('Favorites');
+    parent.style.backgroundColor = 'red';
+    favorites = document.getElementsByClassName('Favorites'); 
+    updateArray(); //Update Favorites
+}
+
+function logFavorites() {
+    for (let i = 0; i < favorites.length; i++) {
+        console.log(favorites[i]);  
+    }
+}
+
+function openFavorites() {
+    // Replace 'https://example.com' with the URL you want to open in the new tab
+    var newTabUrl = 'favorites.html';
+    window.open(newTabUrl, '_blank');
+}
+
+function getFavorites() {
+    return favorites;
+}
